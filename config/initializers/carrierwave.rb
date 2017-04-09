91c2ddd7362de52e005e1fa6c2793ab505ff73dc
小鍛冶泰久 <penmouse275@nifty.com>
2017-04-09 11:59:54 +0900

    upload設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'facebook1129-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/facebook1129-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'facebook1129-develop'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/facebook1129-develop'
   end
 end
